14b787c103a8f6ad427583fa62699bf239283931
april <martinezapril56@yahoo.com>
2019-05-30 17:44:51 -0400

    april

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+import * as types from ' ../constants/ActionTypes'
+
+let nextMessageId = 0
+const nextUserid = 0
+
+export const addMessage = ( message, author ) = ( {
+    type: types. ADD_MESSAGE,
+
+})
